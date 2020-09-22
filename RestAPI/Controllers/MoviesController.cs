f7924e7d9a4431eab1da98c8c851e59ef082cc33
zscholts <zachscholts@quickenloans.com>
2020-09-22 16:19:44 -0400

    initial

---
+++ RestAPI/Controllers/MoviesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestAPI.Controllers
+{
+    [Route("Movie")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        [HttpGet("List")]
+        public List<Movie> List()
+        {
+            List<Movie> movielist = Movie.GetMovies();
+            return movielist;
+        }
+
+        [HttpGet("List/{category}")]
+        public List<Movie> ListCategory(string category)
+        {
+            List<Movie> movielist = Movie.GetMoviesCat(category);
+            if (movielist.Count < 1)
+            {
+                return new List<Movie>();
+            }
+            else
+            {
+                return movielist;
+            }           
+        }
+
+        [HttpGet("Random")]
+        public Movie Random()
+        {
+            List<Movie> movielist = Movie.GetMovies();
+            Movie movie = Movie.GetRandom(movielist);
+            return movie;
+        }
+
+        [HttpGet("Random/{category}")]
+        public Movie RandomCategory(string category)
+        {
+            List<Movie> movielist = Movie.GetMoviesCat(category);
+            if (movielist.Count < 1)
+            {
+                return new Movie();
+            }
+            else
+            {
+                Movie movie = Movie.GetRandom(movielist);
+                return movie;
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ RestAPI/Movie.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper.Contrib;
+using Dapper.Contrib.Extensions;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace RestAPI
+{
+    public class Movie
+    {
+        [Key]
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+
+
+        public static List<Movie> GetMovies()
+        {
+            IDbConnection db = new SqlConnection("Server=.;Database=Movie;user id=dbuser;password=abc123");
+            List<Movie> movielist = db.GetAll<Movie>().ToList();
+            return movielist;
+        }
+
+        public static List<Movie> GetMoviesCat(string category)
+        {
+            IDbConnection db = new SqlConnection("Server=.;Database=Movie;user id=dbuser;password=abc123");
+            List<Movie> movielist = db.Query<Movie>($"SELECT * FROM Movies WHERE Category='{category}'").AsList();
+            return movielist;
+        }
+
+        public static Movie GetRandom(List<Movie> movielist)
+        {
+            IDbConnection db = new SqlConnection("Server=.;Database=Movie;user id=dbuser;password=abc123");
+            int movietotal = movielist.Count;
+            Random random = new Random();
+            Movie movie = movielist[random.Next(1, movietotal)];
+            return movie;
+        }
+
+    }
+
+    public class Movielist
+    {
+        public Movie[] Movies { get; set; }
+    }
+}
